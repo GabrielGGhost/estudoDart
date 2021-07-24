b95b21cf0ed7b2b21343d567c9e3b928181a12eb
Gabriel <gabriel.artioli@epiousion.com.br>
2021-07-24 18:06:29 -0300

    Aula 76 - SWITCH - feito

---
+++ swtich/android/app/src/main/java/com/swtich/MainActivity.java
@@ -0,0 +1,6 @@
+package com.swtich;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ swtich/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+import 'EntradaSwitch.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: EntradaSwitch(),
+    )
+  );
+}
