bdd094a13fc1536d718f667e747ca3d76f231099
Gabriel <gabriel.artioli@epiousion.com.br>
2021-09-11 22:50:22 -0300

    Criado tela inicial de conversas e contatos

--- projeto_whatsapp/lib/Contatos.dart
+++ projeto_whatsapp/lib/Contatos.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:projeto_whatsapp/Entity/eChat.dart';
 
 class Contatos extends StatefulWidget {
   const Contatos({Key? key}) : super(key: key);
@@ -8,8 +9,53 @@ class Contatos extends StatefulWidget {
 }
 
 class _ContatosState extends State<Contatos> {
+
+  List<eChat> chatList = [
+    eChat("Ana Clara",
+        "Olá tudo bem?",
+        "https://firebasestorage.googleapis.com/v0/b/whatsapp-9d115.appspot.com/o/perfil%2Fperfil1.jpg?alt=media&token=05d12251-501b-40e4-abb1-a475f6472950"
+    ),
+    eChat("José Lucas",
+        "Vou ver",
+        "https://firebasestorage.googleapis.com/v0/b/whatsapp-9d115.appspot.com/o/perfil%2Fperfil2.jpg?alt=media&token=f39cf87e-649a-4fb0-9a65-e7c508e98f10"
+    ),
+    eChat("Luciana",
+        "Certo, vou verificar",
+        "https://firebasestorage.googleapis.com/v0/b/whatsapp-9d115.appspot.com/o/perfil%2Fperfil3.jpg?alt=media&token=5e190356-30db-4a75-8f69-88f23c7b4858"
+    ),
+    eChat("Paulo Souza",
+        "Como fazer aquilo?",
+        "https://firebasestorage.googleapis.com/v0/b/whatsapp-9d115.appspot.com/o/perfil%2Fperfil4.jpg?alt=media&token=69aff613-2628-4228-95da-097c8bc3a679"
+    ),
+    eChat("Jamilton Souza",
+        "Como estão as aulas??",
+        "https://firebasestorage.googleapis.com/v0/b/whatsapp-9d115.appspot.com/o/perfil%2Fperfil5.jpg?alt=media&token=bd8f4408-e791-4541-b888-60511dc47d97"
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return ListView.builder(
+        itemCount: chatList.length,
+        itemBuilder: (context, index) {
+          eChat chat = chatList[index];
+
+          return ListTile(
+            contentPadding: EdgeInsets.fromLTRB(16, 8, 16, 8),
+            leading: CircleAvatar(
+              maxRadius: 30,
+              backgroundColor: Colors.grey,
+              backgroundImage: NetworkImage( chat.picturePath ),
+            ),
+            title: Text(
+              chat.name,
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16
+              ),
+            ),
+          );
+        }
+    );
   }
 }
--- projeto_whatsapp/lib/Entity/eChat.dart
+++ projeto_whatsapp/lib/Entity/eChat.dart
@@ -4,20 +4,18 @@ class eChat {
   late String _message;
   late String _picturePath;
 
+  eChat(this._name, this._message, this._picturePath);
+
   String get name => _name;
+  String get message => _message;
+  String get picturePath => _picturePath;
 
   set name(String value) {
     _name = value;
   }
-
-  String get message => _message;
-
-  String get picturePath => _picturePath;
-
   set picturePath(String value) {
     _picturePath = value;
   }
-
   set message(String value) {
     _message = value;
   }
