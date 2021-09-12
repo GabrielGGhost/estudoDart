855277a8ed08a4a800235ad19ced51a151d11e02
Gabriel <gabriel.artioli@epiousion.com.br>
2021-09-11 21:47:12 -0300

    Telas de contato e conversa  adicionadas

---
+++ projeto_whatsapp/lib/Conversas.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Conversas extends StatefulWidget {
+  const Conversas({Key? key}) : super(key: key);
+
+  @override
+  _ConversasState createState() => _ConversasState();
+}
+
+class _ConversasState extends State<Conversas> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
